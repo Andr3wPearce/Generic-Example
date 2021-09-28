2edb5d5e77c034087e40eb8bf60624108c82f1be
Andrew Pearce <anp225@lehigh.edu>
2021-09-28 15:24:34 -0400

    got rid of that nasty public Class Main{.java file

--- public Class Main{.java
+++
@@ -1,9 +0,0 @@
-public Class Main{
-    public static void main(String[] args) {
-        Pair<String, String> p1 = new Pair<>("New York", "NYC");
-
-        Pair<Integer, String> p2 = new Pair<>(12345, "Alice Bark");
-        ArrayList<Pair<Integer, String>> list = new ArrayList<>();
-        
-    }
-}
