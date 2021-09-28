ae42f24652db56d9cf4cad3c6d988c45f11d22eb
Andrew Pearce <anp225@lehigh.edu>
2021-09-28 15:17:40 -0400

    initial commit

---
+++ public Class Main{.java
@@ -0,0 +1,9 @@
+public Class Main{
+    public static void main(String[] args) {
+        Pair<String, String> p1 = new Pair<>("New York", "NYC");
+
+        Pair<Integer, String> p2 = new Pair<>(12345, "Alice Bark");
+        ArrayList<Pair<Integer, String>> list = new ArrayList<>();
+        
+    }
+}
